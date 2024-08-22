9511214494087b7af0946f0ab183d67488815706
Karthikkumar293 <kumarkarthik5665@gmail.com>
2024-08-22 20:28:12 +0530

    Add files via upload

---
+++ karthi1k.py
@@ -0,0 +1,4 @@
+import qrcode
+img = qrcode.make(" insta==its royalkshatria== from ka33 yadgir== Sanatan == jai shree ram== kshatriya ")
+type(img)  # qrcode.image.pil.PilImage
+img.save("insta== its royalkshatria == from ka33 yadgir == Sanatan jai shree ram ==kshatriiya")
